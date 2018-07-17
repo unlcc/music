e1e6e09a1a50eb5337b0aa66832776ce0c61f2a2
lcc <unlcc007@163.com>
2018-07-17 23:10:10 +0800

    test scss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,13 +10,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "./common/scss/variable";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $color-theme;
   margin-top: 60px;
 }
 </style>

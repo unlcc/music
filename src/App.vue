69e090936df66590ca1b2945337e4a78f1a79b01
lcc <unlcc007@163.com>
2018-07-24 22:03:20 +0800

    配置别名alias

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import "./common/scss/variable";
+@import "common/scss/variable";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

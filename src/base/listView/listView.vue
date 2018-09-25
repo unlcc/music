df27673a5c7eb7441aa84e3d5c9186226c819390
lianghancong <lianghancong@aiyunxiao.com>
2018-09-25 22:40:30 +0800

    fixedtitle

--- src/base/listView/listView.vue
+++ src/base/listView/listView.vue
@@ -18,14 +18,22 @@
         </li>
       </ul>
     </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll';
-import {getData} from 'common/js/dom'
+import Loading from 'base/loading/loading';
+import {getData} from 'common/js/dom';
 
 const ANCHOR_HEIGHT = 18;
+const TITLE_HEIGHT = 30;
 
 export default {
   created() {
@@ -37,7 +45,8 @@ export default {
   data() {
     return {
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: 0
     }
   },
   props: {
@@ -47,13 +56,20 @@ export default {
     }
   },
   components: {
-    Scroll
+    Scroll,
+    Loading
   },
   computed: {
     shrotcutList() {
       return this.data.map((group) => {
         return group.title.substr(0, 1);
       });
+    },
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return '';
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : '';
     }
   },
   watch: {
@@ -76,12 +92,21 @@ export default {
         let height2 = listHeight[i + 1];
         if (-newY >= height1 && -newY < height2) {
           this.currentIndex = i;
+          this.diff = height2 + newY;
           return;
         }
       }
 
       // 滚动到底部&&-newY大于最后一个元素上限
       this.currentIndex = listHeight.length - 2;
+    },
+    diff(newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0;
+      if (this.fixedTop === fixedTop) {
+        return;
+      }
+      this.fixedTop = fixedTop;
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`;
     }
   },
   methods: {
@@ -103,6 +128,15 @@ export default {
       this.scrollY = pos.y;
     },
     _scrollTo(index) {
+      if (!index && index !== 0) {
+        return;
+      }
+      if (index < 0) {
+        index = 0;
+      } else if (index > this.listHeight.length - 2) {
+        index = this.listHeight.length - 2;
+      }
+      this.scrollY = -this.listHeight[index];
       this.$refs.listView.scrollToElement(this.$refs.listGroup[index], 200);
     },
     _calculateHeight() {
